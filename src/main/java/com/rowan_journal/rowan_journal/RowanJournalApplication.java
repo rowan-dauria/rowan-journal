3acce3299fb6814f545b9393d440e26e4035b0e9
Rowan d'Auria <56480877+escucheon1@users.noreply.github.com>
2024-07-31 18:35:18 +0100

    initial commit

---
+++ src/main/java/com/rowan_journal/rowan_journal/RowanJournalApplication.java
@@ -0,0 +1,13 @@
+package com.rowan_journal.rowan_journal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RowanJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RowanJournalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rowan_journal/rowan_journal/RowanJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rowan_journal.rowan_journal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RowanJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
